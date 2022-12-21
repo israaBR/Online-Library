cb1bca2e28610fed8d6d2d9b0e1d2a5720f428f8
Israa <israa.abdelaal.2000@gmail.com>
2022-12-21 14:01:23 +0200

    modify location code

--- js/demo.js
+++ js/demo.js
@@ -1,25 +1,19 @@
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (p) {
-        var LatLng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
+    navigator.geolocation.getCurrentPosition(function (position_) {
+        var LatLng = new google.maps.LatLng(position_.coords.latitude, position_.coords.longitude);
         var mapOptions = {
             center: LatLng,
-            zoom: 13,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+            zoom: 15
         };
-        var map = new google.maps.Map(document.getElementById("dvMap"), mapOptions);
+        var map = new google.maps.Map(document.getElementById("map-div"), mapOptions);
         var marker = new google.maps.Marker({
             position: LatLng,
             map: map,
-            title: "<div style = 'height:60px;width:200px'><b>Your location:</b><br />Latitude: " + p.coords.latitude + "<br />Longitude: " + p.coords.longitude
-        });
-        google.maps.event.addListener(marker, "click", function (e) {
-            var infoWindow = new google.maps.InfoWindow();
-            infoWindow.setContent(marker.title);
-            infoWindow.open(map, marker);
+            title: "<div<b>Your location:</b><br />Latitude: " + position_.coords.latitude + "<br />Longitude: " + position_.coords.longitude
         });
     });
 } else {
-    alert('Geo Location feature is not supported in this browser.');
+    console.log();("location is not supported");
 }
 // ----------------------------------------------------------------------------------
 function getUsers(){
